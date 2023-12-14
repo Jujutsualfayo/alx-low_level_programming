fa5a5203c2fb355c63052c34edb265524e37a556
Jujutsualfayo <alphaspaam@gmail.com>
2023-12-14 00:37:42 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h
 /**
- * main -This program prints a line with puts function
+ * main-A program that prints a function using puts
  * Return:0 (Success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
