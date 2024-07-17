c833b114f5bfba8f9a5c0f5d77a1d31f342e4055
Jujutsualfayo <alphaspaam@gmail.com>
2024-07-17 04:08:42 -0700

    search algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-linear.c
+ * Auth: Benjamin Alfayo
+ */
+
+#include "search_algos.h"
+
+/**
+  * linear_search - Searches for a value in an array
+  *    
+  * Description: Prints a value every time it is compared in the array.
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
